a669c8ccfa8939a57c39ac6ad73f77fd22d773e3
Matteo <Matteo.lorenzoq@gmail.com>
2024-09-28 13:49:24 +0200

    update: better h1 animation

--- src/components/animated-heading1.tsx
+++ src/components/animated-heading1.tsx
@@ -1,51 +1,50 @@
 "use client";
 
 import { gasoekOne } from "@/app/font";
-import { cubicBezier, motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 
 const AnimatedHeading1 = () => {
   return ( 
     <h1 className={`${gasoekOne.className} text-5xl md:text-7xl xl:text-[100px]`}>
       <div className="flex items-center gap-8">
-        <span 
-          className="uppercase relative z-20"
-          >
-          404
-          <motion.span
-            initial={{height: "auto"}}
-            animate={{height: 0}}
+        <div className="flex-shrink-0 overflow-hidden">
+          <motion.span 
+            initial={{translateY: 100}}
+            animate={{translateY: 0}}
             transition={{
               duration: .5, 
-              ease: cubicBezier(0.69, 0.14, 0.85, 0.38),
+              ease: easeInOut,
+              delay: 0
             }}
-            className="absolute top-0 left-0 inset-0 bg-[#070707] origin-top"
-          />
-        </span>
+            className="block uppercase relative"
+          >
+            404
+          </motion.span>
+        </div>
         <motion.div 
           className="h-[1px] bg-gray-50 translate-y-2"
           initial={{width: 0}}
           animate={{width: "100%"}}
           transition={{
             duration: .5, 
-            ease: cubicBezier(0.69, 0.14, 0.85, 0.38),
+            ease: easeInOut,
             delay: .6
           }} 
         />
       </div>
-      <div>
-        <span className="uppercase relative">
+      <div className="overflow-hidden">
+        <motion.span 
+          initial={{translateY: 100}}
+          animate={{translateY: 0}}
+          transition={{
+            duration: .5, 
+            ease: easeInOut,
+            delay: .2
+          }}
+          className="block uppercase relative"
+        >
           Devinci
-          <motion.span
-            initial={{height: "auto"}}
-            animate={{height: 0}}
-            transition={{
-              duration: .5,
-              ease: cubicBezier(0.69, 0.14, 0.85, 0.38),
-              delay: .2  
-            }}
-            className="absolute top-0 left-0 inset-0 bg-[#070707] origin-top"
-          />
-        </span>
+        </motion.span>
       </div>
     </h1>
    );
