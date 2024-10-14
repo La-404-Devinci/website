d728deb83cd97921779af0b62221486186b08c8d
Matteo <Matteo.lorenzoq@gmail.com>
2024-10-14 16:18:56 +0200

    feat: intl for not found page + error redirect works on all routes

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,10 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
-import {routing} from './routing';
+import { getRequestConfig } from 'next-intl/server';
+import { redirect } from 'next/navigation';
+import { routing } from './routing';
  
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as 'fr' || "en")) notFound();
+  if (!routing.locales.includes(locale as 'fr' || "en")) redirect("/");
  
   return {
     messages: (await import(`../../messages/${locale}.json`)).default
